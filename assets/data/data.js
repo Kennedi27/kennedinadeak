4ecb422f1a27798528d09a10e0326b3568574722
Kennedi27 <kennedinadeak235@gmail.com>
2022-08-10 21:18:31 +0700

    New Portofolio

---
+++ assets/data/data.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+        "id": 1,
+        "title": "Point Of Sale",
+        "thumbnail": "./assets/project/fruitmart_POS.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and mysql. This program is suitable for use in the store and is very easy to use.",
+        "button": "Source Code",
+        "link_project": "https://github.com/Kennedi27/FruitMart"
+    },
+    {
+        "id": 2,
+        "title": "Beauty Clinik",
+        "thumbnail": "./assets/project/beauty_clinik.png",
+        "content": "is a program designed using the Java programming language that can be used in a beauty clinic.",
+        "button": "Source Code",
+        "link_project": "https://github.com/Kennedi27/Beauty-clinic"
+    },
+    {
+        "id": 3,
+        "title": "Gozibil Mobile Apps",
+        "thumbnail": "./assets/project/Gozibil.png",
+        "content": "is a mobile application built using the React Native framework. This application has features in maximizing waste management.",
+        "button": "Demo",
+        "link_project": "https://youtu.be/xsM2qc6uhgs"
+    },
+    {
+        "id": 4,
+        "title": "Batam Linux User Group",
+        "thumbnail": "./assets/project/Gozibil.png",
+        "content": "is a web-based program designed using Framework Codeigniter 3. This website used to manage all activity BLUG.",
+        "button": "Source Code",
+        "link_project": "https://github.com/Kennedi27/batam_linux.git"
+    },
+    {
+        "id": 5,
+        "title": "Electronics Manufacturing Service (EMS)",
+        "thumbnail": "./assets/project/EMS.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and Sql Server. This program used to manage data from supply chain and the data will convert to data visualisazion. This app is used in PT. Scheneider Electric Manufacturing",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 6,
+        "title": "Analysis Forecast",
+        "thumbnail": "./assets/project/FCEMS.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and Sql Server. This program used to analysis data from supply chain and the data will be calculate on stored procedure. This app is used in PT. Scheneider Electric Manufacturing",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 7,
+        "title": "Pysical Inventory (PI)",
+        "thumbnail": "./assets/project/PICount.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and Sql Server. This program used to inventory in PT. Scheider Electric Batam",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 8,
+        "title": "SEMB Waterspider",
+        "thumbnail": "./assets/project/SEMBWS.png",
+        "content": "is a web-based program designed using the programming languages C#(ASP.Net Core), Javascript and Sql Server. This program used to tracking data kanban in PT. Scheider Electric Batam",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 9,
+        "title": "RAC CTB",
+        "thumbnail": "./assets/project/CTB_RAC.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and Sql Server. This program used to manage data order planing material in PT. Scheider Electric Batam",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 10,
+        "title": "SEMB OWS",
+        "thumbnail": "./assets/project/OWS.png",
+        "content": "is a web-based program designed using the programming languages C#(ASP.Net Core), Javascript and Sql Server. This program used to tracking material production in PT. Scheider Electric Batam",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+    {
+        "id": 11,
+        "title": "Debug Application",
+        "thumbnail": "./assets/project/Debug19.png",
+        "content": "is a web-based program designed using the programming languages PHP, Javascript and Sql Server. This program used to manage data debug material from production in PT. Scheider Electric Batam",
+        "button": "Internal Project",
+        "link_project": "#portofolio"
+    },
+]
